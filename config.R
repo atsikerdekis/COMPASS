70ace5a4630efe007f4729ab6f6cd8dcb3111c7d
Thanos Tsikerdekis <thanos.tsikerdekis@knmi.nl>
2026-09-25 08:57:06 +0000

    update latest, no major changes

--- config.R
+++ config.R
@@ -1,17 +1,17 @@
 ### EXPERIMENT SETTINGS
 ### Experiment 1 (control/reference)
-expname1  <- "iyfb"
-exptype1  <- "AER"
-expclass1 <- "rd"
+expname1  <- "b315"
+exptype1  <- "HAM"
+expclass1 <- "nl"
 ### Experiment 2
-expname2  <- "b315"
+expname2  <- "jbjc"
 exptype2  <- "HAM"
-expclass2 <- "nl"
+expclass2 <- "rd"
 ### Period
 sDate   <- "20190101"
 eDate   <- "20190131"
 ### Runtype
-runtype <- "plot" # plot | download
+runtype <- "download" # plot | download
 ### Other
 massdiag_compare <- FALSE
 
@@ -23,11 +23,11 @@ grib_table_AER <- "config/bins_aerver8.csv"
 grib_table_HAM <- "config/bins_hamm7ver4.0.csv"
 
 ### REGION
-region <- "China"
-
-variables <- c("precip_total","precip_convective","precip_large_scale")
+region <- "global"
 
 ### VARIABLES
+#variables <- c("precip_total","precip_convective","precip_large_scale")
+
 #variables <- c(
 #  "rh"        # ML137, ~surface / ~10 m
 #  "rh_500m",   # ML124, ~500 m
@@ -53,3 +53,23 @@ variables <- c("precip_total","precip_convective","precip_large_scale")
 #  "mss_from_mr_ss","mss_from_mr_du","mss_from_mr_pom","mss_from_mr_bc","mss_from_mr_so4","mss_from_mr_ni","mss_from_mr_am"
 #)
 
+### mss and dep per HAM7 tracer
+variables <- c(
+  "mss_so4_as","mss_so4_ks","mss_so4_cs","mss_so4_ns",
+  "mss_am_as",
+  "mss_ni_as","mss_ni_cs",
+  "mss_bc_as","mss_bc_ki","mss_bc_ks","mss_bc_cs",
+  "mss_pom_as","mss_pom_ki","mss_pom_ks","mss_pom_cs",
+  "mss_soa_ns","mss_soa_ks","mss_soa_as","mss_soa_cs","mss_soa_ki",
+  "mss_ss_as","mss_ss_cs",
+  "mss_du_as","mss_du_ai","mss_du_ci","mss_du_cs",
+
+  "dep_so4_as","dep_so4_ks","dep_so4_cs","dep_so4_ns",
+  "dep_am_as",
+  "dep_ni_as","dep_ni_cs",
+  "dep_bc_as","dep_bc_ki","dep_bc_ks","dep_bc_cs",
+  "dep_pom_as","dep_pom_ki","dep_pom_ks","dep_pom_cs",
+  "dep_soa_ns","dep_soa_ks","dep_soa_as","dep_soa_cs","dep_soa_ki",
+  "dep_ss_as","dep_ss_cs",
+  "dep_du_as","dep_du_ai","dep_du_ci","dep_du_cs"
+)
